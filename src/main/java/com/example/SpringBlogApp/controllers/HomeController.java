5a87cfba378cb25dcb5042c6d751387fd507bcf9
Jovertki <wa9head@gmail.com>
2022-10-20 02:03:33 +0300

    added error handle

--- src/main/java/com/example/SpringBlogApp/controllers/HomeController.java
+++ src/main/java/com/example/SpringBlogApp/controllers/HomeController.java
@@ -7,11 +7,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -48,6 +50,9 @@ public class HomeController {
         }
         if (pageNumber < totalPages) {
             model.addAttribute("nextPage", pageNumber + 1);
+        }
+        if (pageNumber > totalPages) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
         model.addAttribute("currentPage", pageNumber);
         return "home";
